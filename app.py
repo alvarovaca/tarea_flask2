fbbadb5b7894f445af4d33f02874c07493e2277f
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-04-16 17:00:34 +0200

    Página libro.html terminada.

--- app.py
+++ app.py
@@ -9,4 +9,11 @@ with open("books.json") as fichero:
 def inicio():
     return render_template("inicio.html",libros=datos)
 
+@app.route('/libro/<isbn>')
+def libro(isbn):
+    for i in datos:
+        if "isbn" in i.keys() and isbn == i["isbn"]:
+            return render_template("libro.html",libro=i)
+    abort(404)
+
 app.run(debug=True)
